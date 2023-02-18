04db9eef1c50a6c5a922ff99701dfa2b7f480984
bsiegle-kathairos <125082780+bsiegle-kathairos@users.noreply.github.com>
2023-02-17 21:44:52 -0700

    Update Todo.cs

--- DotNetAppSqlDb/Models/Todo.cs
+++ DotNetAppSqlDb/Models/Todo.cs
@@ -5,12 +5,19 @@ namespace DotNetAppSqlDb.Models
 {
     public class Todo
     {
-        public int ID { get; set; }
-        public string Description { get; set; }
+        public string ID { get; set; }
+        public string Customer { get; set; }
+        public string Location { get; set; }
+        public string Site { get; set; }
+        public string Status { get; set; }
+        public string Tank_Size { get; set; }
+        public string Comission_Date { get; set; }
+        public string Telemetry_ID { get; set; }
+        public string GPS { get; set; }
 
-        [Display(Name = "Created Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime CreatedDate { get; set; }
+//         [Display(Name = "Created Date")]
+//         [DataType(DataType.Date)]
+//         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+//         public DateTime CreatedDate { get; set; }
     }
 }
